18f86ba5721b0657958977766ba1a284d1149acb
Ivelisya <138857469+Ivelisya@users.noreply.github.com>
2025-04-19 21:51:13 +0800

    1

---
+++ study/day_1/TestFirstModule/src/test1/TestType.java
@@ -0,0 +1,19 @@
+package test1;
+
+public class TestType {
+    public static void main(String[] args) {
+        byte a = 10; //一个字节8位  - 128 - 127
+        short b = 20;
+        int c = 100000000;// 表数范围
+        float e = 3.14F;
+        long f = 123456789L;
+        double g = 3.14;
+        char h = 'a';//单引号引起来的是单个字符
+        System.out.println("你好");
+        ;
+
+        //布尔类型
+        boolean i = true;
+        boolean j = false;
+    }
+}
