ca1486549f8b9fdeba34cd90748b61a04dd7651c
Ivelisya <138857469+Ivelisya@users.noreply.github.com>
2025-06-29 23:54:27 +0800

    Create Person.java

---
+++ study_/src/com/day04/Person.java
@@ -0,0 +1,15 @@
+package com.day04;
+
+public class Person {
+    //特性-属性-名词 (只定义和业务逻辑相关的代码)
+    String name;
+    int age;
+    double height;
+
+
+    //行为-方法-动词(只定义和业务逻辑相关的代码)
+    //定义一个学习的方法
+    public void study(){
+        System.out.println("青");
+    }
+}
